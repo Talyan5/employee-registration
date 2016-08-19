a03c9aa2077b0e7ab70c682d38cae251b8c76fd8
Anatoliy <anatoliy.starinskiy@gmail.com>
2016-08-18 20:02:29 -0700

    Create Person Class

---
+++ src/main/java/com/as/Person.java
@@ -0,0 +1,26 @@
+/**
+ * File Name: EmployeeRegistration.java<br>
+ * Starisnkiy, Anatoliy<br>
+ * Java Boot Camp Exercise<br>
+ * Instructor: Jean-francois Nepton<br>
+ * Created: Aug 18, 2016
+ */
+
+package com.as;
+
+
+/**
+ * EmployeeRegistration //ADDD (description of class)
+ * <p>
+ * //ADDD (description of core fields)
+ * <p>
+ * //ADDD (description of core methods)
+ *
+ * @author Starinskiy, Anatoliy
+ * @version 1.0.0
+ * @since 1.0
+ */
+public class Person {
+
+
+}
